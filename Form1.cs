1371661292af22ad1c8a63404219487557c1114f
Ravi Saxena <43087386+ravisaxena4181@users.noreply.github.com>
2021-08-24 20:21:27 +0530

    reading data

--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,8 @@ using System.Windows.Forms;
 using PCSC;
 using PCSC.Monitoring;
 using PCSC.Iso7816;
-
+using System.Text;
+using System.Globalization;
 
 namespace SCReader
 {
@@ -33,14 +34,14 @@ namespace SCReader
 
         private void btnAllDevices_Click(object sender, EventArgs e)
         {
-          
+
         }
 
         private void ListAllDevices()
         {
             List<string> devices = new List<string>();
             string devicelist = string.Empty;
-             
+
             var contextFactory = ContextFactory.Instance;
             using (var context = contextFactory.Establish(SCardScope.System))
             {
@@ -83,7 +84,7 @@ namespace SCReader
                 IsStart = true; btnConnectReader.Text = "Stop";
                 StartMonitoring();
             }
-            
+
 
         }
         delegate void SetTextCallback(string text);
@@ -100,7 +101,7 @@ namespace SCReader
             }
             else
             {
-                this.textBox1.Text = text;
+                this.textBox1.Text += Environment.NewLine + text;
             }
         }
         private void SetStart(bool v)
@@ -115,7 +116,7 @@ namespace SCReader
             }
             else
             {
-                this.btnDetectcard.Enabled= v;
+                this.btnDetectcard.Enabled = v;
             }
         }
         private void readcard()
@@ -148,32 +149,98 @@ namespace SCReader
 
         private void btnDetectcard_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var contextFactory = ContextFactory.Instance;
+                using (var ctx = contextFactory.Establish(SCardScope.System))
+                {
+                    using (var isoReader = new IsoReader(ctx, comboBox1.Text,
+                        SCardShareMode.Shared, SCardProtocol.Any, false))
+                    {
 
+                        var apdu = new CommandApdu(IsoCase.Case2Short, isoReader.ActiveProtocol)
+                        {
+                            CLA = 0x00, // Class
+                            Instruction = InstructionCode.GetChallenge,
+                            P1 = 0x00, // Parameter 1
+                            P2 = 0x00, // Parameter 2
+                            Le = 0x08 // Expected length of the returned data
+                        };
+
+                        var response = isoReader.Transmit(apdu);
+                        SetText(string.Format("SW1 SW2 = {0:X2} {1:X2}", response.SW1, response.SW2));
+                        if (response.SW1 == 144 && response.SW2 == 00)
+                        {
+                            if (response.HasData)
+                            {
+                                string dtresp = Encoding.UTF8.GetString(response.GetData());
+                                SetText(string.Format("data = {0:X2}", dtresp));
+                            }
+                            //SetText(string.Format("data = {0:X2}", response.GetData()));
+                        }
+                        else
+                        {
+                            SetText("No data found.");
+                        }
+
+                        //Console.WriteLine("SW1 SW2 = {0:X2} {1:X2}", response.SW1, response.SW2);
+                        // ..
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void case2()
+        {
             var contextFactory = ContextFactory.Instance;
             using (var ctx = contextFactory.Establish(SCardScope.System))
             {
                 using (var isoReader = new IsoReader(ctx, comboBox1.Text, SCardShareMode.Shared, SCardProtocol.Any, false))
                 {
-
+                    string author = "RAVI00000110001";
+                    byte bClass = byte.Parse("A0", NumberStyles.AllowHexSpecifier);
+                    // Convert a C# string to a byte array  
+                    byte[] bytes = Encoding.ASCII.GetBytes(author);
                     var apdu = new CommandApdu(IsoCase.Case2Short, isoReader.ActiveProtocol)
                     {
-                        CLA = 0x00, // Class
-                        Instruction = InstructionCode.GetChallenge,
-                        P1 = 0x00, // Parameter 1
-                        P2 = 0x00, // Parameter 2
-                        Le = 0x08 // Expected length of the returned data
+                        CLA = bClass, // Class
+                        Instruction = InstructionCode.ReadRecord,
+                        P1 = 0x01, // Parameter 1
+                        P2 = 0x04, // Parameter 2
+                        Le = 0x00, // Expected length of the returned data
+                                   //Data= bytes
                     };
+
 
                     var response = isoReader.Transmit(apdu);
                     SetText(string.Format("SW1 SW2 = {0:X2} {1:X2}", response.SW1, response.SW2));
-                    
+                    if (response.SW1 == 0x90 && response.SW2 == 0x00)
+                    {
+                        MessageBox.Show("success");
+                    }
+                    else
+                    {
+                        if (response.HasData)
+                        {
+                            string dtresp = Encoding.UTF8.GetString(response.GetData());
+                            MessageBox.Show(dtresp);
+                        }
+
+
+                    }
                     //Console.WriteLine("SW1 SW2 = {0:X2} {1:X2}", response.SW1, response.SW2);
                     // ..
                 }
 
             }
-
-
         }
 
         private void Monitor_CardRemoved(object sender, CardStatusEventArgs e)
@@ -211,8 +278,8 @@ namespace SCReader
 
         private void StartMonitoring()
         {
-             monitorFactory = MonitorFactory.Instance;
-             monitor = monitorFactory.Create(SCardScope.System);
+            monitorFactory = MonitorFactory.Instance;
+            monitor = monitorFactory.Create(SCardScope.System);
             monitor.CardInserted += Monitor_CardInserted;
             monitor.CardRemoved += Monitor_CardRemoved;
             monitor.Start(selectedCard);//"HID Global OMNIKEY 3x21 Smart Card Reader 0"
@@ -221,7 +288,7 @@ namespace SCReader
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
